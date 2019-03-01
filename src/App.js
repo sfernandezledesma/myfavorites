c277096fd26aba9fbd441e881c37b75807d26fa0
Sebastian Fernandez Ledesma <sfernandezledesma@gmail.com>
2019-03-01 20:13:30 -0300

    ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ErrorBoundary from "./components/ErrorBoundary";
 
 class App extends Component {
   render() {
