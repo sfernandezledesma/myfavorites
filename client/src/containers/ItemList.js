7aefa5a97154f33478a14f9c0dddcc34a9c2141f
Sebastian Fernandez Ledesma <sfernandezledesma@gmail.com>
2019-03-04 20:23:41 -0300

    Ahora las KEYS de ITEMS son String(index)+item.imdbID porque a veces vienen resultados duplicados

--- client/src/containers/ItemList.js
+++ client/src/containers/ItemList.js
@@ -16,7 +16,7 @@ class ItemList extends React.Component {
     return (
       <Grid container justify="center" spacing={spacing} style={{ width: "100%", margin: 0, padding: "1%" }}>
         {items.map((item, index) => (
-          <Grid item key={item.imdbID} sm={6} md={4} lg={4} style={{ flex: "auto" }}>
+          <Grid item key={String(index) + item.imdbID} sm={6} md={4} lg={4} style={{ flex: "auto" }}>
             <Card style={{ display: "flex" }}>
               <Item item={item} />
             </Card>
