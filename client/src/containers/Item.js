53d918eda7cabcce438ef57f86ff47b8575a8a05
Sebastian Fernandez Ledesma <sfernandezledesma@gmail.com>
2019-03-04 23:34:51 -0300

    Bugfix: Detalles de personas

--- client/src/containers/Item.js
+++ client/src/containers/Item.js
@@ -19,7 +19,7 @@ class Item extends React.Component {
         .then(response => response.json())
         .then(data => {
           console.log(data);
-          if (data.vote_count >= 0) {
+          if (data.id) {
             this.setState({ info: data, detailsOpen: true/*, errorMessage: ""*/ });
           } else {
             //this.setState({ errorMessage: data.Error });
@@ -33,15 +33,60 @@ class Item extends React.Component {
     this.setState({ detailsOpen: false });
   }
 
-  render() {
+  renderDetailsCardContent = () => {
     const { item } = this.props;
     const { info } = this.state;
+    if (item.media_type === "person") {
+      return (
+        <Fragment>
+          <Typography variant="subtitle1">
+            {info.name || ""}
+          </Typography>
+          <Typography>
+            {info.birthday ? "Birthday: " + info.birthday : ""}
+          </Typography>
+          <Typography>
+            {info.known_for_department ? "Known for: " + info.known_for_department : ""}
+          </Typography>
+          <Typography gutterBottom>
+            {info.place_of_birth ? "Place of birth: " + info.place_of_birth : ""}
+          </Typography>
+          <Typography align="justify">
+            {info.biography || ""}
+          </Typography>
+        </Fragment>
+      );
+    } else {
+      return (
+        <Fragment>
+          <Typography variant="subtitle1">
+            {info.name || info.title}
+          </Typography>
+          <Typography>
+            {(info.first_air_date || info.release_date || "").split("-")[0]}{" | " + item.media_type.toUpperCase() + (info.runtime ? " | " + info.runtime + " mins" : "")}
+          </Typography>
+          <Typography>
+            {info.genres ? info.genres.map(genre => genre.name).join(", ") : ""}
+          </Typography>
+          <Typography gutterBottom>
+            {info.vote_average ? "TMDb Rating: " + info.vote_average + " | Votes: " + info.vote_count : ""}
+          </Typography>
+          <Typography align="justify">
+            {info.overview || info.biography || ""}
+          </Typography>
+        </Fragment>
+      );
+    }
+  }
+
+  render() {
+    const { item } = this.props;
     if (!this.state.detailsOpen) {
       return (
         <Fragment>
           <CardMedia
             style={{ width: 200, height: 300, flex: "1" }}
-            image={item.poster_path ? "https://image.tmdb.org/t/p/w500" + item.poster_path : "#"}
+            image={(item.poster_path || item.profile_path) ? "https://image.tmdb.org/t/p/w500" + (item.poster_path || item.profile_path) : "#"}
             title={item.name}
           />
           <div style={{ flex: "1", display: "flex", flexDirection: "column" }}>
@@ -50,7 +95,8 @@ class Item extends React.Component {
                 {item.name || item.title}
               </Typography>
               <Typography>
-                {(item.first_air_date || item.release_date || "").split("-")[0]} | {item.media_type.toUpperCase()}{item.origin_country ? " | " + item.origin_country.join(", ") : ""}
+                { item.media_type === "person" ? "PERSON" :
+                  (item.first_air_date || item.release_date || "").split("-")[0]+" | "+item.media_type.toUpperCase()}{item.origin_country ? " | " + item.origin_country.join(", ") : ""}
               </Typography>
             </CardContent>
             <CardActions style={{ flex: "auto", display: "flex", flexDirection: "row-reverse", alignItems: "flex-end" }}>
@@ -65,21 +111,7 @@ class Item extends React.Component {
       return (
         <div style={{ flex: "1", display: "flex", flexDirection: "column" }}>
           <CardContent style={{ flex: "auto" }}>
-            <Typography variant="subtitle1">
-              {info.name || info.title}
-            </Typography>
-            <Typography variant="subtitle1">
-              {(info.first_air_date || info.release_date || "").split("-")[0]} | {item.media_type.toUpperCase() + (info.runtime ? " | " + info.runtime + " mins": "")}
-            </Typography>
-            <Typography>
-              {info.genres.map(genre => genre.name).join(", ")}
-            </Typography>
-            <Typography>
-              TMDb Rating: {info.vote_average} | Votes: {info.vote_count}
-            </Typography>
-            <Typography align="justify">
-              {info.overview || ""}
-            </Typography>
+            {this.renderDetailsCardContent()}
           </CardContent>
           <CardActions style={{ flex: "auto", display: "flex", flexDirection: "row-reverse", alignItems: "flex-end" }}>
             <Button size="small" onClick={this.onDetailsClose}>Back</Button>
