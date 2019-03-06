278c5886c14bc9e81b4e517523786074da68407a
Sebastian Fernandez Ledesma <sfernandezledesma@gmail.com>
2019-03-06 19:03:42 -0300

    Testeando JWT

--- server.js
+++ server.js
@@ -8,10 +8,58 @@ const knex = require('knex');
 const db = require('./connectDB.js').connectDB(knex);
 const app = express();
 const port = process.env.PORT || 5000;
-
+const jwt = require('jsonwebtoken');
+const config = require('./config');
+const middleware = require('./middleware');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+function login(req, res) {
+  let username = req.body.username;
+  let password = req.body.password;
+  // For the given username fetch user from DB
+  let mockedUsername = 'admin';
+  let mockedPassword = 'password';
+
+  if (username && password) {
+    if (username === mockedUsername && password === mockedPassword) {
+      let token = jwt.sign({ username: username },
+        config.secret,
+        {
+          expiresIn: '24h' // expires in 24 hours
+        }
+      );
+      // return the JWT token for the future API calls
+      res.json({
+        success: true,
+        message: 'Authentication successful!',
+        token: token
+      });
+    } else {
+      res.send(403).json({
+        success: false,
+        message: 'Incorrect username or password'
+      });
+    }
+  } else {
+    res.send(400).json({
+      success: false,
+      message: 'Authentication failed! Please check the request'
+    });
+  }
+}
+
+app.post("/login", (req, res) => {
+  login(req, res);
+});
+
+app.get("/getmein", middleware.checkToken, (req, res) => {
+  res.json({
+    success: true,
+    message: 'You are logged in, ' + req.decoded.username
+  });
+});
 
 app.get('/api/search/:language/:title', (req, res) => {
   const { language, title } = req.params;
